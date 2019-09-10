008da275fc57f94f69f0447c2ecb6d43ad213906
liupenglei <liupenglei@xingshulin.com>
2019-09-10 14:48:46 +0800

    add xmind

--- spring-cloud-zuul/src/main/java/org/lpl/zuul/utils/CloudAccountUtils.java
+++ spring-cloud-zuul/src/main/java/org/lpl/zuul/utils/CloudAccountUtils.java
@@ -1,21 +1,26 @@
 package org.lpl.zuul.utils;
 
 import com.alibaba.fastjson.JSONObject;
-
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
+import okhttp3.FormBody;
 import okhttp3.Headers;
-import okhttp3.MediaType;
+import okhttp3.Headers.Builder;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.apache.commons.codec.binary.Base64;
 
 /**
+ * 云账户接口请求组件
+ *
  * @author penglei.liu
  * @version 1.0
  * @date 2019-09-06 17:38
@@ -23,11 +28,49 @@ import org.apache.commons.codec.binary.Base64;
 public class CloudAccountUtils {
 
 
-  private static final String serverRoot = "https://api-jiesuan.yunzhanghu.com";
+  private static final String SERVER_ROOT_URL = "https://api-jiesuan.yunzhanghu.com";
 
-  private static final String url = "/authentication/verify-id";
+  private static final String IDCARD_AUTH_URL = "/authentication/verify-id";
 
-  private static final int TIMEOUT = 5;
+  private static final String BANKCARD_AUTH_URL = "/authentication/verify-bankcard-three-factor";
+
+  private static final String SIGN_TYPE = "sha256";
+
+  private static final String HEADER_KEY_DEALER_ID = "dealer-id";
+
+  private static final String HEADER_KEY_REQUEST_ID = "request-id";
+
+  //  /**
+//   * 身份证号不合法
+//   */
+//  private static final String VERFITY_CODE_IDCARD_ERROR = "10102";
+//  /**
+//   * 姓名输入有误
+//   */
+//  private static final String VERFITY_CODE_NAME_ERROR = "10103";
+//  /**
+//   * 银行卡信息校验失败
+//   */
+//  private static final String VERFITY_CODE_BANK_ERROR = "10104";
+//  /**
+//   * 姓名银行卡不匹配
+//   */
+//  private static final String VERFITY_CODE_NAME_IDCARD_MISMATCH = "10105";
+//  /**
+//   * 身份证号与银行卡号不匹配
+//   */
+//  private static final String VERFITY_CODE_NAME_BANK_MISMATCH = "10106";
+//  /**
+//   * 姓名/身份证号/银行卡号不匹配
+//   */
+//  private static final String VERFITY_CODE_NAME_IDCARD_BANK_ERROR = "10108";
+
+  private static final String dealer_id = "24459062";
+
+  private static final String thirdsKey = "RmuIwVUDKV3zuTp07xR4e60g";
+  private static final String appKey = "6C25pA3d9WLo91z5ppAxEv1oIj3DSG1I";
+
+  private static final int TIMEOUT = 10;
 
   private static final OkHttpClient client = new OkHttpClient()
       .newBuilder()
@@ -35,37 +78,108 @@ public class CloudAccountUtils {
       .readTimeout(TIMEOUT, TimeUnit.SECONDS)
       .build();
 
-
+  /**
+   * 实名认证接口
+   */
   public static void idCardAuth() throws Exception {
 
-    String thirdsKey = "RmuIwVUDKV3zuTp07xR4e60g";
-    String appKey = "6C25pA3d9WLo91z5ppAxEv1oIj3DSG1I";
+    String idCard = "321002197305030914";
+    String userName = "张三1";
 
-    String idCard = "130684199402142272";
-    String userName = "刘朋磊";
-
-    Map<String, String> paramsMap = new HashMap<String, String>();
+    Map<String, String> paramsMap = new HashMap<>();
     paramsMap.put("id_card", idCard);
     paramsMap.put("real_name", userName);
 
-    byte[] content = JSONObject.toJSONString(paramsMap).getBytes("UTF-8");
+    FormBody formBody = createFormBody(JSONObject.toJSONString(paramsMap));
 
-    byte[] enc = CloudAccountDesUtils.TripleDesDecrypt(thirdsKey.getBytes("UTF-8"), content);
-
-    byte[] enc64 = Base64.encodeBase64(enc);
-
-    MediaType jsonType = MediaType.parse("application/json; charset=utf-8");
+    Headers headers = new Builder()
+        .add("dealer-id", "24459062")
+        .add("request-id", UUID.randomUUID().toString().replace("-", ""))
+        .build();
 
     Request request = new Request.Builder()
-        .header("dealer-id", "24459062")
-        .header("request-id", UUID.randomUUID().toString().replace("-", ""))
-        .url(url)
-        .post(RequestBody.create(jsonType, enc64))
+        .headers(headers)
+        .url(SERVER_ROOT_URL + IDCARD_AUTH_URL)
+        .post(formBody)
         .build();
 
     Response execute = client.newCall(request).execute();
-    System.out.println(execute);
+
+    System.out.println(execute.body().string());
 
   }
+
+  /**
+   * 银行卡三要素校验
+   */
+  public static void bankCardAuth() throws Exception {
+    String idCard = "130684199402142272";
+    String userName = "刘朋磊1";
+    String bankCard = "6217000130015406860";
+
+    Map<String, String> paramsMap = new HashMap<>();
+    paramsMap.put("id_card", idCard);
+    paramsMap.put("real_name", userName);
+    paramsMap.put("card_no", bankCard);
+
+    FormBody formBody = createFormBody(JSONObject.toJSONString(paramsMap));
+
+    String requestId = UUID.randomUUID().toString().replace("-", "");
+    Headers headers = new Builder()
+        .add(HEADER_KEY_DEALER_ID, "24459062")
+        .add(HEADER_KEY_REQUEST_ID, requestId)
+        .build();
+
+    Request request = new Request.Builder()
+        .headers(headers)
+        .url(SERVER_ROOT_URL + BANKCARD_AUTH_URL)
+        .post(formBody)
+        .build();
+
+    Response execute = client.newCall(request).execute();
+    ResponseBody body = execute.body();
+    System.out.println(execute.body().string());
+  }
+
+  /**
+   * 构建formBody
+   *
+   * @param requestParams 请求参数
+   * @return 加密后的formBody
+   */
+  public static FormBody createFormBody(String requestParams) throws Exception {
+
+    byte[] content = requestParams.getBytes(StandardCharsets.UTF_8);
+    byte[] enc = CloudAccountDesUtils
+        .TripleDesEncrypt(content, thirdsKey.getBytes(StandardCharsets.UTF_8));
+    byte[] enc64 = Base64.encodeBase64(enc);
+
+    String thirdDesContent = new String(enc64);
+
+    long timestamp = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+    int mess = new Random().nextInt(10000);
+
+    String pre = "data=" + thirdDesContent + "&mess=" + mess +
+        "&timestamp=" + timestamp + "&key=" + appKey;
+
+    String sign = CloudAccountDesUtils.sha256_HMAC(pre, appKey);
+
+    FormBody formBody = new FormBody.Builder()
+        .add("data", thirdDesContent)
+        .add("mess", String.valueOf(mess))
+        .add("timestamp", String.valueOf(timestamp))
+        .add("sign", sign)
+        .add("sign_type", SIGN_TYPE)
+        .build();
+
+    return formBody;
+  }
+
+  public static void main(String[] args) throws Exception {
+    //CloudAccountUtils.idCardAuth();
+
+    CloudAccountUtils.bankCardAuth();
+  }
+
 
 }
