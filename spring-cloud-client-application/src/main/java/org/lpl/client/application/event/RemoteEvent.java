c5a34d2297587f95c9e04306aa316e236a538513
liupenglei <liupenglei@xingshulin.com>
2019-09-20 19:20:28 +0800

    add properties

---
+++ spring-cloud-client-application/src/main/java/org/lpl/client/application/event/RemoteEvent.java
@@ -0,0 +1,10 @@
+package org.lpl.client.application.event;
+
+/**
+ * @author penglei.liu
+ * @version 1.0
+ * @date 2019-09-20 09:48
+ **/
+public class RemoteEvent {
+
+}
--- spring-cloud-server-application/src/main/java/org/lpl/server/application/SpringCloudServerApplication.java
+++ spring-cloud-server-application/src/main/java/org/lpl/server/application/SpringCloudServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.MessageHeaders;
